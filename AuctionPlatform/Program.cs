94e47710f353d1e2f8c1ebf269513121f7a5a0f6
vlerashehuu@gmail.com <vlerashehuu@gmail.com>
2024-01-14 19:41:07 +0100

    app.UseSwaggerUI modified in program.cs

--- AuctionPlatform/Program.cs
+++ AuctionPlatform/Program.cs
@@ -88,11 +88,11 @@ builder.Logging.AddSerilog();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Auction Platform"));
-}
+//if (app.Environment.IsDevelopment())
+//{
+//    app.UseSwagger();
+//    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Auction Platform"));
+//}
 
 app.UseSwagger();
 
